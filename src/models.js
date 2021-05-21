4ce7373ea24bfe7505487c5d6477326810f5dd36
ssan9 <perrsy@gmail.com>
2021-05-21 18:35:12 -0400

    added logic and files to support get by id request

--- src/models.js
+++ src/models.js
@@ -1,136 +1,119 @@
 const { musicians } = require('./musicians');
+var recipes = require('../recipes.json');
 
 const { getSongs } = require('./songs');
 
-function StorageException(message) {
-  // normally es6 classes are not used for throwing error but if you do use these,
-  this.message = message; // then add throw 'new' and call the function
-  this.name = 'StorageException';
-}
-
-function combineData( // combining data from musicians and songs file and displaying in a different structure
-  obj,
-  keyName = 'id',
-  firstName = 'firstName',
-  lastName = 'lastName',
-  genre = 'genre',
-  songs = 'songs'
-) {
-  return Promise.all(
-    // songs file is exporting a function which is using async. With async you should always use
-    Object.keys(obj) // either async/await or promises.Promise all here means we are combining the array data and
-      .filter(key => Object.hasOwnProperty.call(obj, key)) // displaying it asynchronously using promises. To call the
-      .map(async key => {
-        // async function we need to use await. And if we use await in the
-        const res = await getSongs(key).catch(() => []); // current function we need to use async in the beginning of the
-        const keyValue = {}; // block.
-        keyValue[keyName] = key; // Async/await is a cleaner form of using promises
-        keyValue[firstName] = obj[key].firstName; // Catch block holds empty array to take care of error handling
-        keyValue[lastName] = obj[key].lastName; // it means if no song is present for the musicians object then
-        keyValue[genre] = obj[key].genre; // return an empty array
-        keyValue[songs] = res;
-        return keyValue;
-      })
-  );
-}
-
-// alternate function using traditional promises alone (without async/await)
-
-// function objectToKeyValueArray(obj, keyName = 'id', firstName = 'firstName', lastName = 'lastName', genre = 'genre', songs = 'songs') {
-// return Promise.all(Object.keys(obj)
-//     .filter(key => Object.hasOwnProperty.call(obj, key))
-//     .map(key =>
-//         getSongs(key).then(res => {   // so here instead of async/await we use promises
-//           console.log('result', res)
-//           const keyValue = {};
-//           keyValue[keyName] = key;
-//           keyValue[firstName] = obj[key].firstName;
-//           keyValue[lastName] = obj[key].lastName;
-//           keyValue[genre] = obj[key].genre;
-//           keyValue[songs] = res;
-//           return keyValue;
-//         })))
+// function StorageException(message) {
+//   this.message = message;
+//   this.name = 'StorageException';
 // }
 
-// alternative method by using 'of' method
-
-// function objCombine(obj, variable) {
-//   for (let key of Object.keys(obj)) {
-//     if (!variable[key]) variable[key] = {};
-
-//     for (let innerKey of Object.keys(obj[key]))
-//       variable[key][innerKey] = obj[key][innerKey];
-//   }
+// function combineData(
+//   obj,
+//   keyName = 'id',
+//   firstName = 'firstName',
+//   lastName = 'lastName',
+//   genre = 'genre',
+//   songs = 'songs'
+// ) {
+//   return Promise.all(
+//     Object.keys(obj)
+//       .filter(key => Object.hasOwnProperty.call(obj, key))
+//       .map(async key => {
+//         const res = await getSongs(key).catch(() => []);
+//         const keyValue = {};
+//         keyValue[keyName] = key;
+//         keyValue[firstName] = obj[key].firstName;
+//         keyValue[lastName] = obj[key].lastName;
+//         keyValue[genre] = obj[key].genre;
+//         keyValue[songs] = res;
+//         return keyValue;
+//       })
+//   );
 // }
-
-// let combined = {};
-// objCombine(musicians, combined);
-// const songs = await getSongs().catch(() => []);
-// objCombine(songs, combined);
-// console.log(combined)
-// return combined;
 
 /*  eslint func-names: ["error", "as-needed"] */
-const Musicians = {
-  // we create a model called Musicians here
-  /* eslint object-shorthand: ["error", "always", { "ignoreConstructors": true }] */
-  create: (id, firstName, lastName, genre) => {
-    // to use object shorthand - check the repo's - v/final branch; // To add a new item to the Musicians model, just call create with the appropriate arguments. The create method takes 4 parameters, id, firstName, lastName, genre. It then returns an object representing the item.
-    const musician = {
-      id,
-      firstName,
-      lastName,
-      genre,
-    };
-    // merging the passed data with the new id and return the new data when the function is called
-    musicians[musician.id] = musician;
-    return musician;
+const Recipes = {
+  //   /* eslint object-shorthand: ["error", "always", { "ignoreConstructors": true }] */
+  //   create: (id, firstName, lastName, genre) => {
+  //     const musician = {
+  //       id,
+  //       firstName,
+  //       lastName,
+  //       genre,
+  //     };
+  //     // merging the passed data with the new id and return the new data when the function is called
+  //     musicians[musician.id] = musician;
+  //     return musician;
+  //   },
+  // get: () => {
+  //   // combining musicians and songs data on combineData function call and return the data as array of objects when the get method is called
+  //   return combineData(musicians);
+  // },
+  //  get: function() {
+  //   console.log('hi');
+  //   return recipes.map(key => key);
+  // },
+  getById: recipeId => {
+    let { ids } = recipeId;
+    const recipeList = recipes.filter(recipe => recipe.id == ids);
+    // if (recipe.id === ids) {
+    // console.log('hiii', 'recipe.id', recipe.id, 'ids', ids);
+    return recipeList[0].ingredients;
+    // }
+    // return null;
+    // recipes[recipeId.id] = recipeId;
+    // [id] = recipes;
+    // console.log('recipe', recipes);
+    // console.log('recipes', recipes[recipeId.id]);
+    // return recipes[id];
   },
-  get: async () => {
-    // You should get back a JSON response that contains an array of musicians. Each musicians item has the following properties: id, firstname, lastname, genre and songs. We have a single musicians resource that our server exposes at /musicians.
-    // combining musicians and songs data on combineData function call and return the data as array of objects when the get method is called
-    return combineData(musicians); // The get() method is used to retrieve all items that are currently stored in the Musicians.
-  },
-  getById: async musicianId => {
-    const { id } = musicianId;
-    // returning the id, musicians and songs data when the getById function is called if the path id exists in the musicians data
-    const songs = await getSongs(id).catch(() => []);
-    if (musicians[id]) {
-      // here we are checking if params id exists in musicians then merge id, musicians data
-      return {
-        // based on the id and songs, otherwise return null
-        id,
-        ...musicians[id],
-        songs,
-      };
-    }
-    return null;
-  },
-  delete: musicianId => {
-    // The delete() method takes a single argument: the id of the musicians item to be deleted. Note that if you pass this method an id for an item that does not exist, no error will be thrown.
-    // console.log(`Deleting musicians with id \`${musicianId}\``);
-    delete musicians[musicianId];
-  },
-  update: updatedItem => {
-    // Finally, the update() method takes a single parameter: updatedItem which is an object representing the updated item. Note that this method will throw an error if it tries to update a non-existent item.
-    const { id } = updatedItem;
-    if (!(id in musicians)) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because it doesn't exist.`
-      );
-    }
-    // merging the passed data with the existing id and return the updated data when the function is called
-    musicians[updatedItem.id] = updatedItem;
-    return updatedItem;
-  },
+  // getById: async musicianId => {
+  //   const { id } = musicianId;
+  //   console.log('id', id, 'musicians[id]', musicians[id]);
+  //   // returning the id, musicians and songs data when the getById function is called if the path id exists in the musicians data
+  //   const songs = await getSongs(id).catch(() => []);
+  //   if (musicians[id]) {
+  //     return musicians[id];
+  //   }
+  //   return null;
+  // },
+  //  get: () => {
+  //   // combining musicians and songs data on combineData function call and return the data as array of objects when the get method is called
+  //   return combineData(musicians);
+  // },
+  //   getById: async musicianId => {
+  //     const { id } = musicianId;
+  //     // returning the id, musicians and songs data when the getById function is called if the path id exists in the musicians data
+  //     const songs = await getSongs(id).catch(() => []);
+  //     if (musicians[id]) {
+  //       return {
+  //         id,
+  //         ...musicians[id],
+  //         songs,
+  //       };
+  //     }
+  //     return null;
+  //   },
+  //   update: updatedItem => {
+  //     const { id } = updatedItem;
+  //     if (!(id in musicians)) {
+  //       throw new StorageException(
+  //         `Can't update item \`${id}\` because it doesn't exist.`
+  //       );
+  //     }
+  //     // merging the passed data with the existing id and return the updated data when the function is called
+  //     musicians[updatedItem.id] = updatedItem;
+  //     return updatedItem;
+  //   },
 };
 
 function createMusicians() {
-  const storage = Object.create(Musicians);
+  const storage = Object.create(Recipes);
   storage.musicians = {};
   return storage;
 }
 
 module.exports = {
-  Musicians: createMusicians(),
+  Recipes: createMusicians(),
 };
--- src/router.js
+++ src/router.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 // using body-parser's JSON-parser to parse the JSON data sent by clients
 const bodyParser = require('body-parser');
+var recipes = require('../recipes.json');
 
 const jsonParser = bodyParser.json();
 
-const { Musicians } = require('./models'); // importing our musicians model from another module. From the perspective of router.js, Musicians is just an interface we can use to retrieve the current state of our application's musicians list.
+const { Recipes } = require('./models');
 const {
   musiciansSchema,
   musiciansUpdateSchema,
@@ -16,73 +17,104 @@ const {
 const { validation } = require('./schema-validation/validation-middleware');
 
 // getting all musicians
-router.get('/', async (req, res) => {
-  const musicians = await Musicians.get(); // this can also be written as return Musicians.get().then(musicians => res.json(musicians));
-  res.json(musicians); // so whenever we have to call a method which has promise to get the result we need to call it in this format
-}); // in promises the data is available only at the next .then statement, that's why in the combineData funcion we didn't get the value but we are getting it's value here through this .then method or async/await function
-// here we have a server that exposes an endpoint at /. When a GET request is made to that endpoint, our server responds by calling the .get() method on a Musicians model that we have imported. Notice that the API layer doesn't have any conception of how the Musicians model goes about getting the data. The API layer just knows that the Musicians model has a get method that can be called to retrieve a list of stored musicians. Models are where the logic for sourcing and manipulating data should live.
-// When a request is made to /musicians, our app responds by serializing (that is, transforming to JSON) the data returned by Musicians.get(), which will be a list of the current musicians items.
-// Our endpoint knows that it should call Musicians.get(), but it doesn't have any conception of how Musicians.get() is implemented. In general, we want to strive for a thin API layer, concentrating complicated logic in our models
-// We then add individual routes to this router. This code is almost identical to what we used to have in index.js. The key difference is that the endpoint here is just /, not /recipes. Recall that inside the updated index.js, we route requests to /musicians to this router.
-// This handler, in turn, points to the models.js module, and the request is routed to get function in models.js. Finally, the response is sent back to the client in a JSON array that includes each musician.
+// router.get('/', async (req, res) => {
+//   const musicians = await Musicians.get();
+//   res.json(musicians);
+// });
 
-// getting musicians by id
-router.get('/:id', async (req, res) => {
-  try {
-    const musicians = await Musicians.getById(req.params);
-    if (musicians.id) res.json(musicians); // if musician id exists, then only return data
-  } catch (err) {
-    res.status(400).json({
-      errorMessage: 'Musician does not exist',
+// router.get('/', (req, res) => {
+//   console.log('ji');
+//   res.status(200).json(Recipes.get());
+// });
+
+router.get('/shopping-list', (req, res) => {
+  // console.log('req', req.query);
+  // console.log('recipes', recipes, 'recipes.id', recipes.id);
+  // console.log('req.query', req.query, 'req.query.id', req.query.id);
+  // const [recipe] = recipes;
+  // console.log('recipeeeee', recipe)
+  // if (recipe.id) {
+  // console.log('recipeiddd', recipe.id);
+  // const recipeId = recipes.map(recipe => recipe.id)
+  // const recipeId = recipes.filter(recipe => recipe.id === req.query.ids ? recipe.id : null)
+  const arr = [];
+  const recipeId = recipes.forEach(recipe => {
+    console.log('1111', typeof recipe.id, 'jhjgjgjgjgj', typeof req.query.ids);
+    if (recipe.id == req.query.ids) {
+      console.log('iiijjhhjhjgj');
+      arr.push(recipe.id);
+    }
+  });
+
+  console.log('hjgghhfhf', typeof arr[0]);
+
+  // const [...recipeId] = recipes;
+  console.log('recipeId', recipeId, 'req.query.ids', req.query.ids, 'arr', arr);
+  console.log('hhhhhhhhhhhhhhh');
+  let m = arr[0];
+  if (m == req.query.ids) {
+    console.log('m', m, req.query.ids);
+    if (req.query.ids) {
+      console.log('recipes', recipes, 'yooo', recipes.id);
+      return res.json(Recipes.getById(req.query));
+    } else {
+      res.status(400).end();
+    }
+  } else {
+    res.status(404).json({
+      errorMessage: 'NOT_FOUND',
     });
   }
 });
 
-// passing through bodyParser middleware and validation middleware before creating/adding musician data
-router.put('/', jsonParser, validation(musiciansSchema), async (req, res) => {
-  // jsonParser middleware is only usedwhen we
-  const newMusician = await Musicians.create(
-    // have incoming data through post or put requests.
-    req.body.id, // Our PUT endpoint will need to be able to parse data about new musicians items from requests, and it will need to call the Musicians.create to create a new item. It will also need to return a JSON object representing the newly created object and the right status code.
-    req.body.firstName, // When a client makes a POST request to /musicians, we first validate the request body, ensuring that id, firstname, lastname, genre and songs have been supplied. If these values are missing, we log an error and send back a status code 400, along with a helpful error message.
-    req.body.lastName, // If the client has sent valid data, we call the Musicians.create method with data from the request body. create() returns the newly created item, and we respond by sending a 201 status code, along with a JSON object representing the new item.
-    req.body.genre
-  );
-  res.status(201).json(newMusician); // no error message if values are missing? Maybe yup validation is taking care of that
-});
+// getting musicians by id
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const musicians = await Musicians.getById(req.params);
+//     if (musicians.id) res.json(musicians);
+//   } catch (err) {
+//     res.status(400).json({
+//       errorMessage: 'Musician does not exist',
+//     });
+//   }
+// });
 
-router.delete('/:id', async (req, res) => {
-  Musicians.delete(req.params.id); // As with create and read operations, our DELETE endpoint will use the URL /musicians, but will include a path variable for the id of the musicians item to be deleted.
-  res.status(204).end();
-}); // This API endpoint will need to call Musicians.delete(), supplying the id of the item to be deleted. We retrieve the id of the item to be deleted from the request params, and then call Musicians.delete() with that value. We send back a blank response with a 204 status code.
+// passing through bodyParser middleware and validation middleware before creating/adding musician data
+// router.put('/', jsonParser, validation(musiciansSchema), (req, res) => {
+//   const newMusician = Musicians.create(
+//     req.body.id,
+//     req.body.firstName,
+//     req.body.lastName,
+//     req.body.genre
+//   );
+//   res.status(201).json(newMusician);
+// });
 
 // updating musician data by id
-router.put(
-  // To update existing musicians items, we'll reuse our /musicians endpoint, but we'll add a path variable for the id of the to-be-updated resource.
-  '/:id', // Like our POST endpoint, our PUT endpoint will need to be able to parse data about updated musicians items. It will need to validate the data sent by the client, and if all is right, call Musicians.update with the updated data.
-  jsonParser,
-  validation(musiciansUpdateSchema), // validation schema middleware to give errors if parameters with wrong data types are passed in the body
-  async (req, res) => {
-    try {
-      // calling the update function to update the old values with the new values if the values of path id and request body id match
-      Musicians.update({
-        // For put requests no need to put id in the body when sending data to update
-        id: req.params.id, // like our PUTT endpoint, we're validating the data to ensure a.) that the required fields have been sent, and b.) that the id values in the request body and request path URL match. Assuming the request is valid, this endpoint calls Musicians.update() with the updated data. In the success case, this endpoint returns a 200 HTTP status code with the targeted id
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        genre: req.body.genre,
-        songs: req.body.songs,
-      });
+// router.put(
+//   '/:id',
+//   jsonParser,
+//   validation(musiciansUpdateSchema),
+//   async (req, res) => {
+//     try {
+//       // calling the update function to update the old values with the new values if the values of path id and request body id match
+//       Musicians.update({
+//         id: req.params.id,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         genre: req.body.genre,
+//         songs: req.body.songs,
+//       });
 
-      return res.status(200).json({
-        id: `${req.params.id}`,
-      });
-    } catch (err) {
-      return res.status(400).json({
-        errorMessage: err,
-      });
-    }
-  }
-);
+//       return res.status(200).json({
+//         id: `${req.params.id}`,
+//       });
+//     } catch (err) {
+//       return res.status(400).json({
+//         errorMessage: err,
+//       });
+//     }
+//   }
+// );
 
 module.exports = { router };
